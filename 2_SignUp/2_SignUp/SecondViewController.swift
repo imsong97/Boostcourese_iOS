b5781d71a8b1fa53ca580eea9bff476304123b96
DyNamiC <imsong1997@gmail.com>
2021-05-13 22:13:23 +0900

    SignUp ~ 3.2

--- 2_SignUp/2_SignUp/SecondViewController.swift
+++ 2_SignUp/2_SignUp/SecondViewController.swift
@@ -11,8 +11,44 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        print("SecondViewController - view가 메모리에 로드 됨")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("SecondViewController - view가 화면에 보여질 예정")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("SecondViewController - view가 화면에 보여짐")
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        print("SecondViewController - view가 화면에서 사라질 예정")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        print("SecondViewController - view가 화면에서 사라짐")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        print("SecondViewController - view가 subview를 레이아웃 하려고 함")
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        print("SecondViewController - view가 subview를 레이아웃 함")
     }
     
     // 네비게이션 -> POP 버튼 누르면 이전화면 보여지게 (코드로)
